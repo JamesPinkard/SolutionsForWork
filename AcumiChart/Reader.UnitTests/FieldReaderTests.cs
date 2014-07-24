890314e6640b42557ce79c9f78985bf35b80b495
JamesPinkard <James.b.Pinkard@gmail.com>
2014-07-24 18:25:00 -0500

    Made AcumiChart
    A redesign for ExcelSolution.

---
+++ AcumiChart/Reader.UnitTests/FieldReaderTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using System.ComponentModel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Reader.UnitTests
+{
+    [TestFixture]
+    public class ExcelFielderTests
+    {
+        [Test]
+        public void ExcelFielder_ReadExcelFileWithFieldsAtTopRow_ReturnTopRow()
+        {
+            new
+        }
+    }
+}
