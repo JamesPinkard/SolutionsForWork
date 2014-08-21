f34c7fbae3035342344d2396698c993c794e427c
JamesPinkard <James.b.Pinkard@gmail.com>
2014-08-21 10:02:36 -0500

    Settting Up Integration Test for Sheet Factory

---
+++ FileLinkTracker/FileLinkTrackerTests/FactoryOfSheetsTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ExcelSharp;
+
+namespace ExcelSharpTests
+{
+    [Category("Factory Sheet Tests")]
+    [TestFixture]
+    public class FactoryOfSheetsTests
+    {
+        ExcelOperator testOperator;
+        Workbook testWorkbook;
+        TestHelper helper;
+
+        #region Setup and Teardown
+        [TestFixtureSetUp]
+        public void InitializeWorksheet()
+        {
+            helper.InitializeWorksheet(testOperator, testWorkbook);
+        }
+
+        [TestFixtureTearDown]
+        public void CloseExcel()
+        {
+            helper.CloseExcel(testOperator);
+        }
+
+        [SetUp]
+        public void ClearCells()
+        {
+            helper.ClearCells(testWorkbook);
+        }
+        #endregion        
+
+        /* TODO
+        [Test]
+        public void FactoryOfTableSheets_IntegrationTest();
+        ISheetFactory tableSheetFactory = new TableSheetFactory(testWorkbook);
+        Sheet tableSheet = new TableSheet();
+        tableSheet = tableSheetFactory.MakeSheet();
+
+
+        SheetFactory linkSheetFactory = new LinkSheetFactory(testWorkbook);
+        linkSheetFactory.Overwrite();
+        */
+    }
+}
---
+++ FileLinkTracker/FileLinkTrackerTests/TestHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelSharp;
+
+namespace ExcelSharpTests
+{
+    public static class TestHelper
+    {
+        public void InitializeWorksheet(ExcelOperator testOperator, Workbook testWorkbook )
+        {
+            testOperator = new ExcelOperator();
+            testOperator.InitializeExcel();
+            testWorkbook = initTestWorkbook(testOperator);
+        }
+
+        public void CloseExcel(ExcelOperator testOperator)
+        {
+            testOperator.CloseWorkbook();
+            testOperator.CloseExcel();
+        }
+
+        public void ClearCells(Workbook testWorkbook)
+        {
+            testWorkbook.ActiveSheet.Cells.Clear();
+        }
+
+        private Workbook initTestWorkbook(ExcelOperator testOperator)
+        {
+            Workbook testWorkbook = testOperator.OpenWorkbook();
+            return testWorkbook;
+        }
+    }
+}
