1f02c4a45c23b66588d3dc58833ad5428723862e
JamesPinkard <James.b.Pinkard@gmail.com>
2014-08-25 15:09:08 -0500

    Added Support Testing to ExcelSharp

---
+++ FileLinkTracker/FileLinkTrackerTests/ISheetCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelSharpTests
+{
+    interface ISheetCommand
+    {
+        public void ExecuteWithWorkbook();
+        public void ExecuteWithSheet();
+    }
+}
