2f767c19cb79a966b3ee6c23b36594011eb86620
JamesPinkard <James.b.Pinkard@gmail.com>
2014-09-18 14:02:31 -0500

    Fixed header inputs set by Git hub

--- FileLinkTracker/ExcelSharpTests/WriterTests.cs
+++ FileLinkTracker/ExcelSharpTests/WriterTests.cs
@@ -147,9 +147,7 @@ namespace ExcelSharpTests
 
             // Execute
             directoryWriter.Write();
-
-            GetSubDirectoryAndSubFilesInSheet();
-            
+            GetSubDirectoryAndSubFilesInSheet();            
 
             Assert.That(subFilesInSheet, Is.EquivalentTo(subFiles));
             Assert.That(subDirectoryInSheet, Is.EqualTo(subDirectories[0].FullName));
@@ -168,7 +166,7 @@ namespace ExcelSharpTests
                 // Get File Count
                 fileCount = getDirectoryFileCount();
 
-<<<<<<< HEAD
+
                 // Get Sub Directories
                 myTestDirectory = getSolutionDirectory();
                 subDirectories = myTestDirectory.GetDirectories();
@@ -200,9 +198,8 @@ namespace ExcelSharpTests
         }
         
         // TODO Test that sheet has sub directory hyperlinks
-=======
-        // TODO Test that sheet has hyperlinks
->>>>>>> origin/master
+        
+
         // TODO Error if link writer is set twice
         // Message: Use a sheet factory or change sheet command
         //  to initialize sheet properly.
